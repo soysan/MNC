de9d9fcd74fb2ffe24e2506788079ae2fc8d4d7f
Masayuki <soy2.san@gmail.com>
2021-05-03 23:28:48 +0900

    add core.js for test

---
+++ src/js/core.js
@@ -0,0 +1,11 @@
+import Data from './Data';
+
+/**
+ * @param {String|HTMLElement} selector to wrap
+ */
+
+const GreenLeaves = selector => {
+    if (!(this instanceof GreenLeaves)) {
+        return new GreenLeaves(selector);
+    }
+}
