73c26cb28181f5e544332a19e07fe96ba5e63090
Masayuki <soy2.san@gmail.com>
2021-05-25 23:31:42 +0900

    compile vir webpack

---
+++ src/js/index.js
@@ -0,0 +1,1 @@
+import "../sass/mnc.scss"
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,52 @@
-module.exports = {
-    // 仮js entry
-    entry: `./src/js/index.js`,
+const enabledSourceMap = process.env.NODE_ENV !== 'production';
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+module.exports = {
+    entry: `./src/js/index.js`,
     output: {
-        // OSS->/distに出す
-        path: `${__dirname}/dist`,
+        path: path.resolve(__dirname, 'dist/css'),
         filename: "main.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            url: false,
+                            sourceMap: enabledSourceMap,
+                        },
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require('sass'),
+                            // sassOptions: {
+                            //     fiber: false,
+                            // },
+                            sourceMap: enabledSourceMap,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "mnc.css",
+        }),
+    ],
+    devtool: "source-map",
+    watchOptions: {
+        ignored: /node_modules/
     },
     // {development,production,none} のちに変える
     mode: "development"
